76dc9d05a96e1851b69d7d99b6717bce94f02cc7
Zan Lin <oaobj4@gmail.com>
2024-04-09 23:25:10 +0800

    add progressbar package

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,14 +1,16 @@
+import time
+import progressbar 
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value=1000000)
 with open('reviews.txt', 'r') as file:
     for line in file:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
-            print(len(data))
+        bar.update(count) # 使用第三方套件(讀取進度條)
 print('檔案讀取完，共有', len(data), '筆資料')
 
-# 文字計數
 sum_len = 0
 for ele in data:
     sum_len = sum_len + len(ele)
@@ -19,15 +21,15 @@ for ele in data:
     if len(ele) < 100:
         short.append(ele)
 print('共有', len(short), '筆長度小於100的留言')
-print(short[0])
 
 good = []
 for ele in data:
     if 'good' in ele:
         good.append(ele)
 print('共有', len(good), '筆提到good的留言')
-print(good[0])
 
+# 文字計數
+start_time = time.time()
 wc = {} # word_count
 for d in data:
     words = d.split(' ')
@@ -36,6 +38,8 @@ for d in data:
             wc[word] += 1 # 該字的count +1
         else:
             wc[word] = 1 # 新增新的key進wc字典
+end_time = time.time()
+print('花了', end_time - start_time, 'seconds')
 
 # 印出出現過1000000次的單字
 for word in wc:
